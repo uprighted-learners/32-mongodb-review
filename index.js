9b112bf3e30c3c295cdae1eb21834f1f4e9aec6a
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-17 18:28:38 -0500

    full CRUD API functionality for blog posts

--- index.js
+++ index.js
@@ -54,6 +54,79 @@ const blogPostSchema = new mongoose.Schema({
 // Create a model for Blog Post
 const BlogPost = mongoose.model('BlogPost', blogPostSchema);
 
+// GET - /api/blogposts - get all blog posts
+app.get("/api/blogposts", async (req, res) => {
+    try {
+        const posts = await BlogPost.find({});
+        res.json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: "No blog posts found" });
+    }
+})
+
+// GET - /api/blogposts/:id - get a blog post by the id
+app.get("/api/blogposts/:id", async (req, res) => {
+    try {
+        const post = await BlogPost.findById(req.params.id);
+        res.json(post);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: "Blog post not found" });
+    }
+})
+
+// POST - /api/blogposts - create a new blog post
+app.post("/api/blogposts", async (req, res) => {
+    const post = new BlogPost({
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body
+    })
+
+    try {
+        const newPost = await post.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: error.message });
+    }
+})
+
+// PUT - /api/blogposts/:id - update a blog post by id
+app.put("/api/blogposts/:id", async (req, res) => {
+    try {
+        const post = await BlogPost.findById(req.params.id);
+        if (post) {
+            post.title = req.body.title;
+            post.author = req.body.author;
+            post.body = req.body.body;
+            const updatedPost = await post.save();
+            res.json(updatedPost);
+        } else {
+            res.status(404).json({ message: "Blog post not found" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: error.message });
+    }
+});
+
+// DELETE - /api/blogposts/:id - delete a blog post by id
+app.delete("/api/blogposts/:id", async (req, res) => {
+    try {
+        const post = await BlogPost.findByIdAndDelete(req.params.id);
+        if (!post) {
+            res.status(404).json({ message: "Blog post not found" });
+            return;
+        }
+        res.json({ message: "Blog post deleted" });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ message: error.message });
+    }
+});
+
 // spin up the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
